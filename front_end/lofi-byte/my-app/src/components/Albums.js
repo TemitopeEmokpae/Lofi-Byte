e6697d596fb8e0ac2f66e0f5ad8f9e6cc6999210
TemitopeEmokpae <temokpae@oswego.edu>
2022-06-30 14:59:59 -0400

    adding code for db

--- front_end/lofi-byte/my-app/src/components/Albums.js
+++ front_end/lofi-byte/my-app/src/components/Albums.js
@@ -1,3 +1,23 @@
-export default function Albums(){
-   return <h1>album</h1>
+import React from 'react'
+
+const Albums=(props)=>{
+
+    const showList=({albums})=>{
+        if(albums){
+            return albums.map((item,index)=>{
+                return(
+                    <img key={index} src={`/images/albums/${item.cover}.jpg`}/>
+                )
+            })
+        }
+    }
+
+    return(
+        <div className="albums">
+            {showList(props)}
+        </div>
+    )
+
 }
+
+export default AlbumList;
