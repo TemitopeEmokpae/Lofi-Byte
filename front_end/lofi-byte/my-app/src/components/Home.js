d1f0b35b58d33d4276671c784613ac50ef61a346
Ko Ko Aung <kaung@oswego.edu>
2022-06-30 22:32:34 -0400

    database test

--- front_end/lofi-byte/my-app/src/components/Home.js
+++ front_end/lofi-byte/my-app/src/components/Home.js
@@ -1,3 +1,12 @@
+import "../css/Home.css"
+
 export default function Home(){
-   return <h1>home</h1>
+   return (
+    <div className="background">
+    <div className="moto">
+        <h1> Welcome To The Chill Area</h1>
+        <p className="names">project by Ko Ko Aung and Temitope Emokpae</p>
+    </div>
+    </div>
+    )
 }
--- front_end/lofi-byte/my-app/src/components/NavBar.js
+++ front_end/lofi-byte/my-app/src/components/NavBar.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react'
 import {Link, useMatch, useResolvedPath} from 'react-router-dom'
 import '../css/NavBar.css'
+import JSONDATA from '../Artist.json'
+import { useState } from 'react'
 export default function NavBar(){
+    const [searchTerm, setSearchTerm] = useState('');
         return (
+            <div>
             <nav className='nav_item'>
             <Link to='/' className='title' style={{color: '#D6DDF1', paddingLeft: '20px'}}>Lofi-byte</Link>
             <div className='menu-icon'></div>
@@ -13,6 +17,24 @@ export default function NavBar(){
                 <CustomLink to= "/Artists">Artists</CustomLink>
             </ul>
             </nav>
+            <div className='displayS'>
+            <input type={'text'} placeholder= "Search.."  className='bar' onChange={event => {setSearchTerm(event.target.value);}}/>
+            {JSONDATA.filter((val)=>{
+                if(searchTerm == ""){
+                    return "";
+                } else if(val.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
+                    return val.first_name;
+                }
+            }).map((val, key)=>{
+                return(
+                    <div className='Search' key={key}>
+                        {/* <CustomLink to={"/" + val.}>Home</CustomLink> */}
+                        <p>{val.first_name}</p>
+                    </div>
+                );
+            })}
+            </div>
+            </div>
           )
   
 }
