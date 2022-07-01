0d9449b2fd7b2d4f8b8c1b8870894db92de21885
TemitopeEmokpae <temokpae@oswego.edu>
2022-06-30 20:22:30 -0400

    chamged back page

--- front_end/lofi-byte/my-app/src/components/Albums.js
+++ front_end/lofi-byte/my-app/src/components/Albums.js
@@ -1,23 +1,3 @@
-import React from 'react'
-
-const Albums=(props)=>{
-
-    const showList=({albums})=>{
-        if(albums){
-            return albums.map((item,index)=>{
-                return(
-                    <img key={index} src={`/images/albums/${item.cover}.jpg`}/>
-                )
-            })
-        }
-    }
-
-    return(
-        <div className="albums">
-            {showList(props)}
-        </div>
-    )
-
+export default function Albums(){
+   return <h1>album</h1>
 }
-
-export default AlbumList;
